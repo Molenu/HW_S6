d52c917a126633026c729d37e6c577a1138e6eda
Дарья <120316778+Molenu@users.noreply.github.com>
2022-12-29 23:18:02 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.0, 7, 8, -2, -2 -> 21, -7, 567, 89, 223-> 3
+
+ Console.WriteLine("Введите сколько будет чисел");
+ int num = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+int count = 0;
+
+ while (i<=num)
+ {
+    Console.WriteLine("Введите число");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+   
+    i++;
+    
+  if (num1>0)
+{
+                 count++;
+  }
+}
+Console.WriteLine("Количество чисел больше нуля = "+ count);
